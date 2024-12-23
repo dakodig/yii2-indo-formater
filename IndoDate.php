ff5bf70908306234fe535a9db7a6b599f8c2abef
Ade Priyanto <mr.adepriyanto@gmail.com>
2024-12-24 05:45:55 +0700

    Update IndoDate

--- IndoDate.php
+++ IndoDate.php
@@ -21,16 +21,30 @@ class IndoDate extends Model
 
     public static function create($datetime,$format = 'Y-m-d H:i:s')
     {
-        $exp = explode(" ", $datetime);
         $datetime = new \DateTime();
-        if(count($exp)==1){
-            $exp1 = explode("-", $exp[0]);
-            $result =  $datetime->setDate($exp1[0],$exp1[1],$exp1[2])->format($format);
-        }else if(count($exp)>1){
-            $exp1 = explode("-", $exp[0]);
-            $exp2 = $exp[1];
-            $result = $datetime->setDate($exp1[0],$exp1[1],$exp1[2])->format($format).' '.$exp2;
+        if(preg_match('/[- :]/', $datetime)) {
+            if(preg_match('/[ ]/', $datetime)) {
+                $exp = explode(" ", $datetime);
+                if (count($exp) == 1) {
+                    $exp1 = explode("-", $exp[0]);
+                    $result = $datetime->setDate($exp1[0], $exp1[1], $exp1[2])->format($format);
+                } else if (count($exp) > 1) {
+                    $exp1 = explode("-", $exp[0]);
+                    $exp2 = $exp[1];
+                    $result = $datetime->setDate($exp1[0], $exp1[1], $exp1[2])->format($format) . ' ' . $exp2;
+                }
+            }else if(preg_match('/[-]/', $datetime) && !preg_match('/[ ]/', $datetime)) {
+                $exp = explode("-", $datetime);
+                if(count($exp)==2){
+                    $result = $datetime->setDate($exp[0], $exp[1])->format($format);
+                }else{
+                    $result = $datetime->setDate($exp[0], $exp[1],$exp[2])->format($format);
+                }
+            }
+        }else{
+            $result = 'error format input';
         }
+
         return $result;
     }
 }
